11f1a5a2492f9b9a2bebad69c0865b72a1718c27
kess <enikabe@yahoo.fr>
2020-10-30 11:30:44 +0100

    recuperer et enregistré en bdd

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -48,12 +51,31 @@ class AccountController extends AbstractController
      *
      * @return Response
      */
-    public function register()
+    public function register(Request $request,EntityManagerInterface $manager,UserPasswordEncoderInterface $encoder)
     {
 
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class,$user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            // encoder le mot de passe
+            $hash = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($hash);
+
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre compte a été bien enregistré'
+            );
+
+            $this->redirectToRoute("account_login");
+        }
         
         return $this->render('account/register.html.twig',[
             'form'=>$form->createView()
